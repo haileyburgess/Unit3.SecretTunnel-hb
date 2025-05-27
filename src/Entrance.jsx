2195b05782ec23d2298388e1ef43a1e5a2a9484c
Hailey Burgess <hailey.burgess@redcross.org>
2025-05-27 18:14:09 -0400

    step 2 kind of

--- src/Entrance.jsx
+++ src/Entrance.jsx
@@ -1,7 +1,17 @@
+import { useAuth } from "./AuthContext";
+
 /** Users can enter their name to receive a token from the API. */
 export default function Entrance() {
   // TODO: call signup when form is submitted
+  const { signup } = useAuth();
 
+  const setToken = (formData) => {
+    console.log(formData);
+  
+    const username = formData.get("name");
+    console.log(username);
+    signup(username);
+  };
   return (
     <>
       <h1>Cave Entrance</h1>
@@ -15,7 +25,7 @@ export default function Entrance() {
         fixed on you. The one on the left opens its mouth, and with a deep,
         rumbling voice, it asks, "Who approaches? Speak your name."
       </p>
-      <form>
+      <form action={setToken}>
         <label>
           Name
           <input name="name" />
@@ -24,4 +34,5 @@ export default function Entrance() {
       </form>
     </>
   );
-}
+};
+
